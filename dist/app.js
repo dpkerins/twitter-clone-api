617a5fa38fa65536aa6f532c684eedf74d6e0fa0
Douglas <douglaskerins@gmail.com>
2022-02-28 09:38:15 +0000

    adding get tweets route

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('This is a different message.');
 });
+app.get('/tweets/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const tweets = yield prisma.tweet.findMany();
+    res.json(tweets);
+    // res.send('This is a tweet message.');
+}));
 app.listen(5000, () => {
     console.log('Server is running');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
-import express, { Application, Request, Response, NextFunction} from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello');
+  res.send('This is a different message.');
 });
+
+app.get('/tweets/', async (req: Request, res: Response) => {
+  const tweets = await prisma.tweet.findMany();
+  res.json(tweets)
+  // res.send('This is a tweet message.');
+})
 
 app.listen(5000, () => {
   console.log('Server is running');
